2ef07709e90d98d62b59ebb6154fe53d71881a04
Your Name <you@example.com>
2019-01-07 18:48:31 +0200

    js edited for class

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 var svgLayout = document.getElementById("svgLayout");
 var x = 0;
 var posX = 0;
-var sqaure = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+var square = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
 var startbtn = document.getElementById("start");
 var timer = document.getElementById("time");
 var time = document.getElementsByClassName("time-show");
 
-startbtn.addEventListener("click", function(){
+
+class Square {
+    constructor(square, x, y, length, width, fill) {
+        this.square = square;
+        this.x = x;
+        this.y = y;
+        this.length = length;
+        this.width = width;
+        this.fill = fill;
+    }
+    drawSquare() {
+        this.square.setAttribute('x', this.x);
+        this.square.setAttribute('y', this.y);
+        this.square.setAttribute('width', this.width);
+        this.square.setAttribute('height', this.length);
+        this.square.setAttribute('fill', this.fill);
+        this.square.setAttribute('id', "sq");
+        svgLayout.appendChild(this.square);
+    }
+}
+
+
+
+startbtn.addEventListener("click", function () {
+    
+    //create square object
+    var sq = new Square(square, 0, 470, 50, 50, "#ee76ad");
+    sq.drawSquare();
+
     startbtn.style.display = "none";
-    time[0].style.display="block";
+    time[0].style.display = "block";
     timer.style.display = "flex";
     var fiveMinutes = 60 * 1,
-    display = document.querySelector('#time');
+        display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 });
-    
+
 function startTimer(duration, display) {
+
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10)
@@ -33,28 +62,18 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-function drawSquare(sqaure, x, y, width, height, fill) {
-    sqaure.setAttribute('x', x);
-    sqaure.setAttribute('y', y);
-    sqaure.setAttribute('width', width);
-    sqaure.setAttribute('height', height);
-    sqaure.setAttribute('fill', fill);
-    // rect.setAttribute('stroke', "black");
-    sqaure.setAttribute('id', "sq");
-    svgLayout.appendChild(sqaure);
-}
-
-drawSquare(sqaure, 0, 470, 50, 50, "#ee76ad");
 
 window.addEventListener("keydown", function (e) {
     if (e.keyCode === 39 && posX < 450) {
         posX += 10;
-        drawSquare(sqaure, posX, 470, 50, 50, "#ee76ad");
+        sq = new Square(square, posX, 470, 50, 50, "#ee76ad");
+        sq.drawSquare();
         console.log(posX);
     }
     if (e.keyCode === 37 && posX > 0) {
         posX -= 10;
-        drawSquare(sqaure, posX, 470, 50, 50, "#ee76ad");
+        sq = new Square(square, posX, 470, 50, 50, "#ee76ad");
+        sq.drawSquare();
         console.log(posX);
     }
 })
